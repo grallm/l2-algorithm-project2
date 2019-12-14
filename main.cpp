01cf31d98b5aae0198ce14b3e50b996144ee3182
Malo Grall <malo.grall@gmail.com>
2019-12-14 23:16:10 +0100

    Added extraireCroissance()

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ typedef struct _datum{
     DATATYPE valeur;
     _datum * suiv;
 } data;
-typedef _datum * p_data;
+typedef _datum * p_data; // Changed to _datum to have auto-completion (because data is ambigous)
 
 
 /** TODO
@@ -41,7 +41,7 @@ p_data ajoutDevant(DATATYPE uneval, p_data chain){
     p_data maille;
     
     // Début
-    maille = new data;
+    maille = new _datum; // Changed to _datum to no have error (because data is ambigous)
     
     maille->valeur = uneval;
     maille->suiv = chain;
@@ -102,8 +102,11 @@ p_data saisieNombre(int nb){
 // Les 2 chaînes initiales seront désordonnées, l'une des 2 têtes sera au milieu de la chaîne
 // Version itérative
 /** Préconditions
+ * Les 2 chaînes doivent être triées en ordre croissant
 */
-// Postconditions
+/** Postconditions
+ * Le pointeur passées en 2e argument de la fonction renvoie vers une maille de la chaîne résultante, il n'est donc plus utilisable comme tête de chaîne
+*/
 p_data fusion(p_data head, p_data middle){
   // Variables
   p_data left, right, actual; // Elément où l'on est rendu sur la chaîne
@@ -189,18 +192,24 @@ int nbCroissances(p_data chain){
   bool inMonot;
 
   // Début
-  nb = 0;
-  inMonot = false;
+  nb = 0; // Compteur de monotonies
+  inMonot = false; // Vérifier si on est sur une croissance
+
+  // Tester toutes les mailles
   while (chain != nullptr)
   {
+    // Si on commence une nouvelle monotonie, si c'est le cas on ajoute 1 au compteur
     if(!inMonot){
       inMonot = true;
       nb++;
     }
+    // Vérifier si l'on n'a pas plus de mailles
     if(chain != nullptr){
+      // Si la valeur suivante est inférieure strictement on change de monotonie
       if((*chain).suiv != nullptr && (*chain).valeur > (*(*chain).suiv).valeur){
         inMonot = false;
       }
+      // Maille suivante
       chain = (*chain).suiv;
     }
   }
@@ -209,12 +218,28 @@ int nbCroissances(p_data chain){
   // Fin
 }
 
+// Place dans le chaînage mono la première monotonie croissante de chain et l’en retire
+void extraireCroissance(p_data & chain, p_data & mono){
+    // Variables
+    p_data headRemChain; // Tête du chaînon qui va être retiré
+    p_data tailRemChain; // Queue du chaîne qui va être retiré
 
-/** TODO
-int nbCroissances(p_data chain){}
-
-void extraireCroissance(p_data & chain, p_data & mono){}
-*/
+    // Début
+    headRemChain = chain;
+    // Si la chaîne est vide on ne fait rien
+    if(chain != nullptr){
+      // Aller jusqu'à la fin de la monotonie
+      while ((*chain).suiv != nullptr && (*chain).valeur <= (*(*chain).suiv).valeur)
+      {
+        chain = (*chain).suiv;
+      }
+      tailRemChain = chain; // Récupérer la dernière maille
+      chain = (*chain).suiv; // Le retirer de la chaîne chain
+      (*tailRemChain).suiv = mono; // Ajouter le chaînon à mono
+      mono = headRemChain; // Retourner le nouveau mono
+    }
+    // Fin
+}
 
 
 int main(){
@@ -235,6 +260,7 @@ int main(){
     test->valeur = 1;
     test = ajoutDerriere(3, ajoutDerriere(2, test));
     aff(test); */
+    // aff(nullptr);
     
     
     // Tests saisieBorne()
@@ -252,14 +278,24 @@ int main(){
     // aff(fusion(saisieNombre(2), saisieNombre(2))); // Plusieurs élements par chaîne Ok
     // aff(fusion(nullptr, saisieNombre(3))); // 1 chaîne avec des éléments, l'autre pas Ok
     // aff(fusion(saisieNombre(1), saisieNombre(3))); // 1 chaîne avec 1 élément, l'autre plusieurs Ok
+    // aff(fusion(nullptr, nullptr); // Ok
     
     // Tests fusionR()
     // aff(fusionR(saisieNombre(1), saisieNombre(1))); // 1 seul élement par chaîne Ok
     // aff(fusionR(saisieNombre(2), saisieNombre(2))); // Plusieurs élements par chaîne Ok
     // aff(fusionR(nullptr, saisieNombre(3))); // 1 chaîne avec des éléments, l'autre pas Ok
     // aff(fusionR(saisieNombre(1), saisieNombre(3))); // 1 chaîne avec 1 élément, l'autre plusieurs Ok
+    // aff(fusionR(nullptr, nullptr); // Ok
 
 
     // Tests nbCroissances()
-    cout << nbCroissances(saisieBorne(0)) << endl;
+    // cout << nbCroissances(saisieBorne(0)) << endl;
+    // cout << nbCroissances(nullptr) << endl;
+
+
+    // Tests extraireCroissance()
+    /* p_data chain1 = saisieNombre(2), chain2 = saisieNombre(2);
+    extraireCroissance(chain1, chain2);
+    aff(chain1);
+    aff(chain2); */
 }
